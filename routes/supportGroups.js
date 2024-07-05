afb6f46756fa0943cb271448d1a496861d59e5eb
frukt12 <frukt@tutamail.com>
2024-07-06 00:14:08 +0300

    Add user profile

---
+++ routes/supportGroups.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const router = express.Router()
+const controller = require('../controllers/supportGroupsControllers')
+
+router.get("/",controller.getAllSupportGroups)
+router.get("/reports/getall/:startDate&:endDate",controller.getAllSupportGroupsForReports)
+router.get("/:id",controller.getSupportGroupById)
+router.post("/",controller.createNew)
+router.put("/update",controller.updateSupportGroup)
+router.delete("/delete",controller.delete)
+
+
+module.exports = router
